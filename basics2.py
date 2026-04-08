cc339d56cfe1a701faf63c3a68d8245eee17364d
mokshitvora <138001979+mokshitvora@users.noreply.github.com>
2026-04-08 23:55:59 +0530

    Day 4 - Tavily search tool working

--- basics2.py
+++ basics2.py
@@ -4,16 +4,6 @@ from dotenv import load_dotenv
 import os
 
 
-def ask(system,model,message):
-    load_dotenv()
-    client = Groq(api_key=os.getenv("GROQ_API_KEY"))
-    
-    response = client.chat.completions.create(
-    model=model,
-    messages=[
-        {"role": "system", "content": system},
-        {"role": "user", "content": message}
-    ])
     return response.choices[0].message.content
 
 
